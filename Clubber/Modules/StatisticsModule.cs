190b59b26e6aea9889bfec6d64a95e80fe3b6a45
Spoertm <spoertm@gmail.com>
2020-12-26 13:04:42 +0100

    Change logic, add overloads

--- Clubber/Modules/StatisticsModule.cs
+++ Clubber/Modules/StatisticsModule.cs
@@ -15,6 +15,7 @@ namespace Clubber.Modules
 	{
 		private readonly ChartHelper ChartHelper;
 		private readonly Dictionary<int, ulong> ScoreRolesDict;
+		private const int MAX_LIMIT = 229900;
 
 		public StatisticsModule(ChartHelper _chartHelper, ScoreRoles _scroreRoles)
 		{
@@ -24,24 +25,53 @@ namespace Clubber.Modules
 
 		[Command("every10")]
 		[Summary("Shows number of users per 10s PB bracket.\n\n`bottomLimit`: The larger number/further down the leaderboard (default = 229900)\n\n`topLimit`: The smaller number/further up the leaderboard (default = 1)")]
-		public async Task Every10(uint bottomLimit = 229900, uint topLimit = 1) => await EveryX(10, (int)bottomLimit, (int)topLimit);
+		public async Task Every10(uint topLimit, uint bottomLimit)
+			=> await EveryX(10, (int)bottomLimit, (int)topLimit);
+
+		[Command("every10")]
+		public async Task Every10(uint bottomLimit)
+			=> await EveryX(10, (int)bottomLimit, 1);
+
+		[Command("every10")]
+		public async Task Every10()
+			=> await EveryX(10, MAX_LIMIT, 1);
+
 
 		[Command("every50")]
 		[Summary("Shows number of users per 50s PB bracket.\n\n`bottomLimit`: The larger number/further down the leaderboard (default = 229900)\n\n`topLimit`: The smaller number/further up the leaderboard (default = 1)")]
-		public async Task Every50(uint bottomLimit = 229900, uint topLimit = 1) => await EveryX(50, (int)bottomLimit, (int)topLimit);
+		public async Task Every50(uint topLimit, uint bottomLimit)
+			=> await EveryX(50, (int)bottomLimit, (int)topLimit);
+
+		[Command("every50")]
+		public async Task Every50(uint bottomLimit)
+			=> await EveryX(50, (int)bottomLimit, 1);
+
+		[Command("every50")]
+		public async Task Every50()
+			=> await EveryX(50, MAX_LIMIT, 1);
+
 
 		[Command("every100")]
 		[Summary("Shows number of users per 100s PB bracket.\n\n`bottomLimit`: The larger number/further down the leaderboard (default = 229900)\n\n`topLimit`: The smaller number/further up the leaderboard (default = 1)")]
-		public async Task Every100(uint bottomLimit = 229900, uint topLimit = 1) => await EveryX(100, (int)bottomLimit, (int)topLimit);
+		public async Task Every100(uint topLimit, uint bottomLimit)
+			=> await EveryX(100, (int)bottomLimit, (int)topLimit);
 
-		public async Task EveryX(int bracketSize, int bottomLimit, int topLimit)
+		[Command("every100")]
+		public async Task Every100(uint bottomLimit)
+			=> await EveryX(100, (int)bottomLimit, 1);
+
+		[Command("every100")]
+		public async Task Every100()
+			=> await EveryX(100, 229900, 1);
+
+		private async Task EveryX(int bracketSize, int bottomLimit, int topLimit)
 		{
 			if (bottomLimit < topLimit)
 			{
 				await ReplyAsync("The bottom limit cant be smaller than the upper limit.");
 				return;
 			}
-			if (bottomLimit > 229900 || topLimit < 1)
+			if (bottomLimit > MAX_LIMIT || topLimit < 1)
 			{
 				await ReplyAsync("Bottom limit can't be larger than 229900 and top limit can't be smaller than 1.");
 				return;
@@ -111,7 +141,6 @@ namespace Clubber.Modules
 			}
 
 			await Context.Channel.SendFileAsync(chartStream, "chart.png");
-
 			await processingMessage.DeleteAsync();
 		}
 	}
