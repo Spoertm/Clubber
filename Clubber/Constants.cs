63b1f88826dc7f4b897db6b702b444c530c4ba0d
Spoertm <spoertm@gmail.com>
2021-01-06 18:55:55 +0100

    Add constants

--- Clubber/Constants.cs
+++ Clubber/Constants.cs
@@ -3,5 +3,7 @@
 	public static class Constants
 	{
 		public const ulong CheaterRoleId = 693432614727581727;
+		public const ulong MembersRoleId = 728663492424499200;
+		public const ulong NoScoreRoleId = 458375331468935178;
 	}
 }
