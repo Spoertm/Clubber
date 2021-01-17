6f4c767028ad0828584973ea70359a35303d0a8b
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-01-17 18:05:28 +0100

    Create CustomException.cs

---
+++ Clubber/Database/CustomException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clubber.Database
+{
+	public class CustomException : Exception
+	{
+		public CustomException(string? message, Exception innerException)
+			: base(message, innerException)
+        {
+        }
+	}
+}
