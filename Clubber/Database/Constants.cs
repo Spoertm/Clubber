9157f2763e66572bda5f7816e8381d8b35d9053b
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-01-18 17:52:17 +0100

    Update Constants.cs

--- Clubber/Database/Constants.cs
+++ Clubber/Database/Constants.cs
@@ -5,7 +5,7 @@ namespace Clubber
 	public static class Constants
 	{
 		public const string Prefix = "+";
-		public const string Token = "NzQzNDMxNTAyODQyMjk4MzY4.XzUkig.UQrKlF7axeeFqewonpkTTAwaIIo"; //"NzA0ODM1NjA3NzA4MzAzNDMy.Xqi7UA.mE7eM-kVW7F7nrLs5vcvb8J1GHA";
+		public const string Token = "NzQzNDMxNTAyODQyMjk4MzY4.XzUkig.UQrKlF7axeeFqewonpkTTAwaIIo";
 		public const ulong CheaterRoleId = 693432614727581727;
 		public const ulong WrRoleId = 446688666325090310;
 		public const ulong Top3RoleId = 472451008342261820;
