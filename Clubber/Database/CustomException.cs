492a722dfc0819372521ad1dc592c4c6aeb5c4b7
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-01-18 15:33:12 +0100

    Add overload

--- Clubber/Database/CustomException.cs
+++ Clubber/Database/CustomException.cs
@@ -4,6 +4,11 @@ namespace Clubber.Database
 {
 	public class CustomException : Exception
 	{
+		public CustomException(string? message)
+			: base(message)
+		{
+		}
+
 		public CustomException(string? message, Exception innerException)
 			: base(message, innerException)
         {
