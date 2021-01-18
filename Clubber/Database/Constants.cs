12f1535ea668290ecdb2d3b5dfccf2a51d7fafed
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-01-18 16:23:38 +0100

    Put useless roles into a list

--- Clubber/Database/Constants.cs
+++ Clubber/Database/Constants.cs
@@ -7,12 +7,12 @@ namespace Clubber
 		public const string Prefix = "+";
 		public const string Token = "NzQzNDMxNTAyODQyMjk4MzY4.XzUkig.UQrKlF7axeeFqewonpkTTAwaIIo"; //"NzA0ODM1NjA3NzA4MzAzNDMy.Xqi7UA.mE7eM-kVW7F7nrLs5vcvb8J1GHA";
 		public const ulong CheaterRoleId = 693432614727581727;
-		public const ulong MembersRoleId = 728663492424499200;
 		public const ulong WrRoleId = 446688666325090310;
 		public const ulong Top3RoleId = 472451008342261820;
 		public const ulong Top10RoleId = 556255819323277312;
-		public const ulong NoScoreRoleId = 458375331468935178;
 		public const ulong ClubberExceptionsChannel = 800012212532936714;
+
+		public static readonly List<ulong> UselessRoles = new() { 728663492424499200, 458375331468935178 };
 
 		public static readonly Dictionary<int, ulong> ScoreRoles = new()
 		{
