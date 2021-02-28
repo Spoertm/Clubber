9edeccbd0eeab33dd3737b1b98adb546a16e3ca2
Spoertm <spoertm@gmail.com>
2021-02-28 03:00:27 +0100

    Add statsf command

--- Clubber/Database/Constants.cs
+++ Clubber/Database/Constants.cs
@@ -52,5 +52,26 @@ To speed this up, you can manually update your own or someone else's roles by us
 			[3] = 472451008342261820,
 			[10] = 556255819323277312,
 		};
+
+		public static readonly Dictionary<short, string> DeathtypeDict = new()
+		{
+			{ 0, "FALLEN" },
+			{ 1, "SWARMED" },
+			{ 2, "IMPALED" },
+			{ 3, "GORED" },
+			{ 4, "INFESTED" },
+			{ 5, "OPENED" },
+			{ 6, "PURGED" },
+			{ 7, "DESECRATED" },
+			{ 8, "SACRIFICED" },
+			{ 9, "EVISCERATED" },
+			{ 10, "ANNIHILATED" },
+			{ 11, "INTOXICATED" },
+			{ 12, "ENVENOMATED" },
+			{ 13, "INCARNATED" },
+			{ 14, "DISCARNATED" },
+			{ 15, "ENTANGLED" },
+			{ 16, "HAUNTED" },
+		};
 	}
 }
--- Clubber/Modules/Stats.cs
+++ Clubber/Modules/Stats.cs
@@ -3,14 +3,15 @@ using Clubber.Helpers;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using System;
 using System.Threading.Tasks;
 
 namespace Clubber.Modules
 {
 	[Name("Info")]
-	[Alias("me")]
-	[Group("stats")]
-	[Summary("Provides statistics from the leaderboard for users that are in this server and registered.")]
+	[Group("me")]
+	[Alias("stats", "statsf", "statsfull")]
+	[Summary("Provides statistics from the leaderboard for users that are in this server and registered.\n`statsf` shows all the information available.")]
 	public class Stats : AbstractModule<SocketCommandContext>
 	{
 		[Command]
@@ -21,7 +22,7 @@ namespace Clubber.Modules
 		[Command]
 		[Remarks("stats clubber\nstats <@743431502842298368>")]
 		[Priority(2)]
-		public async Task StatsFromName([Name("name | tag")] [Remainder] string name)
+		public async Task StatsFromName([Name("name | tag")][Remainder] string name)
 		{
 			(bool success, SocketGuildUser? user) = await FoundOneUserFromName(name);
 			if (success && user != null)
@@ -44,23 +45,45 @@ namespace Clubber.Modules
 				return;
 
 			LeaderboardUser lbPlayer = await DatabaseHelper.GetLbPlayer((uint)DatabaseHelper.DdUsers.Find(du => du.DiscordId == user!.Id)!.LeaderboardId);
+			EmbedBuilder statsEmbed = new();
 
-			Embed statsEmbed = new EmbedBuilder()
+			if (Context.Message.Content.StartsWith("+statsf") || Context.Message.Content.StartsWith("+statsfull"))
 			{
-				Title = $"Stats for {user!.Username}",
-				Description =
+				TimeSpan t = TimeSpan.FromSeconds((double)lbPlayer.TimeTotal / 10000);
+				statsEmbed.Description =
+$@"✏️ Leaderboard name: {lbPlayer.Username}
+🛂 Leaderboard ID: {lbPlayer.Id}
+⏱ Score: {lbPlayer.Time / 10000f:0.0000}s
+🥇 Rank: {lbPlayer.Rank}
+💀 Kills: {lbPlayer.Kills}
+💀 Lifetime kills: {lbPlayer.KillsTotal:N0}
+♦️ Gems: {lbPlayer.Gems}
+♦️ Lifetime gems: {lbPlayer.GemsTotal:N0}
+⏱ Total time alive: {t.TotalSeconds:N}s ({t.TotalHours:F0}h {t.Minutes:F0}m {t.Seconds}s)
+🗡 Daggers hit: {lbPlayer.DaggersHit:N0}
+🗡 Daggers fired: {lbPlayer.DaggersFired:n0}
+🎯 Accuracy: {(double)lbPlayer.DaggersHit / lbPlayer.DaggersFired * 100:0.00}%
+🗡 Total daggers hit: {lbPlayer.DaggersHitTotal:N0}
+🗡 Total daggers fired: {lbPlayer.DaggersFiredTotal:N0}
+🎯 Lifetime accuracy: {(double)lbPlayer.DaggersHitTotal / lbPlayer.DaggersFiredTotal * 100:0.00}%
+😵 Total deaths: {lbPlayer.DeathsTotal}
+😵 Death type: {Constants.DeathtypeDict[lbPlayer.DeathType]}";
+			}
+			else
+			{
+				statsEmbed.Description =
 $@"✏️ Leaderboard name: {lbPlayer.Username}
 🛂 Leaderboard ID: {lbPlayer.Id}
 ⏱ Score: {lbPlayer.Time / 10000f:0.0000}s
 🥇 Rank: {lbPlayer.Rank}
 💀 Kills: {lbPlayer.Kills}
 ♦️ Gems: {lbPlayer.Gems}
-🎯 Accuracy: {(double)lbPlayer.DaggersHit / lbPlayer.DaggersFired * 100:0.00}%",
-				ThumbnailUrl = user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl(),
+🎯 Accuracy: {(double)lbPlayer.DaggersHit / lbPlayer.DaggersFired * 100:0.00}%";
 			}
-			.Build();
 
-			await ReplyAsync(null, false, statsEmbed, null, AllowedMentions.None, new MessageReference(Context.Message.Id));
+			statsEmbed.WithTitle($"Stats for {user!.Username}").WithThumbnailUrl(user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl());
+
+			await ReplyAsync(null, false, statsEmbed.Build(), null, AllowedMentions.None, new MessageReference(Context.Message.Id));
 		}
 	}
 }
