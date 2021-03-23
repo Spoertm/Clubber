7c09a86ff2dfccfb5f4e39e854aa9d0dd012bf05
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-03-23 21:25:58 +0100

    Use roles Dictionaries only in the class that needs them

--- Clubber/Helpers/UpdateRolesHelper.cs
+++ Clubber/Helpers/UpdateRolesHelper.cs
@@ -13,6 +13,37 @@ namespace Clubber.Helpers
 {
 	public class UpdateRolesHelper
 	{
+		private static readonly Dictionary<int, ulong> _scoreRoles = new()
+		{
+			[1200] = 626477161825697803,
+			[1180] = 728017461911355454,
+			[1160] = 626476794878623756,
+			[1140] = 626476562128044052,
+			[1120] = 525082045614129163,
+			[1100] = 402530230109208577,
+			[1075] = 525733825934786570,
+			[1050] = 399577125180669963,
+			[1025] = 525967813551325196,
+			[1000] = 399570979610820608,
+			[950] = 728017240762482788,
+			[900] = 399570895741386765,
+			[800] = 399570790506299398,
+			[700] = 399570712018288640,
+			[600] = 399569864261632001,
+			[500] = 399569581561217024,
+			[400] = 399569447771439104,
+			[300] = 399569332532674562,
+			[200] = 399569259182948363,
+			[100] = 399569183966363648,
+			[0] = 461203024128376832,
+		};
+		private static readonly List<ulong> _uselessRoles = new() { 728663492424499200, 458375331468935178 };
+		private static readonly Dictionary<int, ulong> _rankRoles = new()
+		{
+			[1] = 446688666325090310,
+			[3] = 472451008342261820,
+			[10] = 556255819323277312,
+		};
 		private readonly DatabaseHelper _databaseHelper;
 		private readonly WebService _webService;
 
@@ -115,28 +146,28 @@ namespace Clubber.Helpers
 
 		private static (IEnumerable<ulong> ScoreRoleToAdd, IEnumerable<ulong> ScoreRolesToRemove) HandleScoreRoles(IEnumerable<ulong> userRolesIds, int playerTime)
 		{
-			KeyValuePair<int, ulong> scoreRole = Constants.ScoreRoles.FirstOrDefault(sr => sr.Key <= playerTime / 10000);
+			KeyValuePair<int, ulong> scoreRole = _scoreRoles.FirstOrDefault(sr => sr.Key <= playerTime / 10000);
 
 			List<ulong> scoreRoleToAdd = new();
 			if (!userRolesIds.Contains(scoreRole.Value))
 				scoreRoleToAdd.Add(scoreRole.Value);
 
-			IEnumerable<ulong> filteredScoreRoles = Constants.ScoreRoles.Values.Where(rid => rid != scoreRole.Value).Concat(Constants.UselessRoles);
+			IEnumerable<ulong> filteredScoreRoles = _scoreRoles.Values.Where(rid => rid != scoreRole.Value).Concat(_uselessRoles);
 			return (scoreRoleToAdd, userRolesIds.Intersect(filteredScoreRoles));
 		}
 
 		private static (IEnumerable<ulong> TopRoleToAdd, IEnumerable<ulong> TopRolesToRemove) HandleTopRoles(IEnumerable<ulong> userRolesIds, int rank)
 		{
-			KeyValuePair<int, ulong>? rankRole = Constants.RankRoles.FirstOrDefault(rr => rank <= rr.Key);
+			KeyValuePair<int, ulong>? rankRole = _rankRoles.FirstOrDefault(rr => rank <= rr.Key);
 
 			List<ulong> topRoleToAdd = new();
 			if (rankRole.Value.Value == 0)
-				return new(topRoleToAdd, userRolesIds.Intersect(Constants.RankRoles.Values));
+				return new(topRoleToAdd, userRolesIds.Intersect(_rankRoles.Values));
 
 			if (!userRolesIds.Contains(rankRole.Value.Value))
 				topRoleToAdd.Add(rankRole.Value.Value);
 
-			IEnumerable<ulong> filteredTopRoles = Constants.RankRoles.Values.Where(rid => rid != rankRole.Value.Value);
+			IEnumerable<ulong> filteredTopRoles = _rankRoles.Values.Where(rid => rid != rankRole.Value.Value);
 			return new(topRoleToAdd, userRolesIds.Intersect(filteredTopRoles));
 		}
 	}
