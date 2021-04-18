15cf900b29108412d05f7df0958222a3dbca9be5
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-04-18 17:50:40 +0200

    Refactor and cleanup

--- Clubber/Modules/ExtendedModulebase.cs
+++ Clubber/Modules/ExtendedModulebase.cs
@@ -3,7 +3,6 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,7 +11,7 @@ namespace Clubber.Modules
 	public abstract class ExtendedModulebase<T> : ModuleBase<T>
 		where T : SocketCommandContext
 	{
-		public async Task<bool> IsError(bool condition, string output)
+		protected async Task<bool> IsError(bool condition, string output)
 		{
 			if (!condition)
 				return false;
@@ -21,10 +20,10 @@ namespace Clubber.Modules
 			return true;
 		}
 
-		public async Task<IMessage> InlineReplyAsync(string message, bool ping = false)
-			=> await ReplyAsync(message, isTTS: false, embed: null, options: null, allowedMentions: ping ? null : AllowedMentions.None, messageReference: new MessageReference(Context.Message.Id));
+		protected async Task InlineReplyAsync(string message, bool ping = false)
+			=> await ReplyAsync(message, isTTS: false, embed: null, options: null, allowedMentions: ping ? null : AllowedMentions.None, messageReference: new(Context.Message.Id));
 
-		public async Task<(bool Success, SocketGuildUser? User)> FoundUserFromDiscordId(ulong discordId)
+		protected async Task<(bool Success, SocketGuildUser? User)> FoundUserFromDiscordId(ulong discordId)
 		{
 			SocketGuildUser? user = Context.Guild.GetUser(discordId);
 
@@ -34,28 +33,28 @@ namespace Clubber.Modules
 			return (false, null);
 		}
 
-		public async Task<(bool Success, SocketGuildUser? User)> FoundOneUserFromName(string name)
+		protected async Task<(bool Success, SocketGuildUser? User)> FoundOneUserFromName(string name)
 		{
 			string trimmedName = name.TrimStart('<', '@', '!').TrimEnd('>');
 
-			if (ulong.TryParse(trimmedName, out ulong userID) && Context.Guild.GetUser(userID) is SocketGuildUser guildUser)
+			if (ulong.TryParse(trimmedName, out ulong userId) && Context.Guild.GetUser(userId) is { } guildUser)
 				return (true, guildUser);
 
-			IEnumerable<SocketGuildUser> userMatches = Context.Guild.Users.Where(u =>
-				!u.IsBot &&
-				(u.Username.Contains(name, StringComparison.InvariantCultureIgnoreCase) ||
-				u.Nickname?.Contains(name, StringComparison.InvariantCultureIgnoreCase) == true));
+			SocketGuildUser[] userMatches = Context.Guild.Users
+				.Where(u =>
+					!u.IsBot &&
+					(u.Username.Contains(name, StringComparison.InvariantCultureIgnoreCase) ||
+					u.Nickname?.Contains(name, StringComparison.InvariantCultureIgnoreCase) == true))
+				.ToArray();
 
-			if (await IsError(!userMatches.Any(), "User not found."))
+			if (await IsError(userMatches.Length == 0, "User not found."))
 				return (false, null);
 
-			if (userMatches.Count() > 1)
-			{
-				await ReplyAsync(message: null, isTTS: false, embed: EmbedHelper.MultipleMatches(userMatches, name), options: null, allowedMentions: AllowedMentions.None, messageReference: new MessageReference(Context.Message.Id));
-				return (false, null);
-			}
+			if (userMatches.Length == 1)
+				return (true, userMatches.FirstOrDefault());
 
-			return (true, userMatches.FirstOrDefault());
+			await ReplyAsync(message: null, isTTS: false, embed: EmbedHelper.MultipleMatches(userMatches, name), options: null, allowedMentions: AllowedMentions.None, messageReference: new(Context.Message.Id));
+			return (false, null);
 		}
 	}
 }
