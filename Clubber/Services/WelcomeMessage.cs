0ddd9b8aa864ec0948406292613220ed55206636
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-06-18 20:52:58 +0200

    Remove unnecessary attribute

--- Clubber/Services/WelcomeMessage.cs
+++ Clubber/Services/WelcomeMessage.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 
 namespace Clubber.Services
 {
-	[RequireContext(ContextType.Guild)]
 	public class WelcomeMessage
 	{
 		private readonly DatabaseHelper _databaseHelper;
