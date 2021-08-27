f11443982a9544b45a54a5f57a8ece0eab3032aa
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-08-27 19:03:13 +0200

    Fix channel ID typo

--- Clubber/Helpers/DiscordHelper.cs
+++ Clubber/Helpers/DiscordHelper.cs
@@ -1,4 +1,5 @@
-﻿using Discord;
+﻿using Clubber.Configuration;
+using Discord;
 using Discord.WebSocket;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace Clubber.Helpers
 
 		protected DiscordHelper(DiscordSocketClient client)
 		{
-			_clubberExceptionsChannel = client.GetChannel(1123123) as SocketTextChannel;
+			_clubberExceptionsChannel = client.GetChannel(Config.ClubberExceptionsChannelId) as SocketTextChannel;
 		}
 
 		public static async Task LogExceptionEmbed(Embed embed)
