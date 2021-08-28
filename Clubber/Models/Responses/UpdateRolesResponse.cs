6ffb28585c687ccf144c75eb7cb509dd720cb80e
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-08-28 22:38:58 +0200

    Generalise field in record

--- Clubber/Models/Responses/UpdateRolesResponse.cs
+++ Clubber/Models/Responses/UpdateRolesResponse.cs
@@ -1,7 +1,7 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
 using System.Collections.Generic;
 
 namespace Clubber.Models.Responses
 {
-	public sealed record UpdateRolesResponse(bool Success, SocketGuildUser? User, IEnumerable<ulong>? RolesAdded, IEnumerable<ulong>? RolesRemoved);
+	public sealed record UpdateRolesResponse(bool Success, IGuildUser? User, IEnumerable<ulong>? RolesAdded, IEnumerable<ulong>? RolesRemoved);
 }
