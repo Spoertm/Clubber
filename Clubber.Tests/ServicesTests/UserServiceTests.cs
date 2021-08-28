e8da3083d3d12ddf63930a9343f205cd9f12f2e7
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-08-28 19:30:05 +0200

    Add UserService test

---
+++ Clubber.Tests/ServicesTests/UserServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Clubber.Configuration;
+using Clubber.Helpers;
+using Clubber.Models;
+using Clubber.Models.Responses;
+using Clubber.Services;
+using Discord;
+using Moq;
+using Xunit;
+
+namespace Clubber.Tests.ServicesTests
+{
+	public class UserServiceTests
+	{
+		private readonly UserService _sut;
+		private readonly Mock<IDatabaseHelper> _databaseHelperMock;
+
+		public UserServiceTests()
+		{
+			_databaseHelperMock = new();
+			_sut = new(_databaseHelperMock.Object);
+		}
+
+		[Fact]
+		public void IsValidForRegistrationDetectsBot()
+		{
+			Mock<IGuildUser> guildUser = new();
+			guildUser.SetupGet(user => user.IsBot).Returns(true);
+			UserValidationResponse response = _sut.IsValidForRegistration(guildUser.Object, true);
+			Assert.True(response.IsError);
+		}
+	}
+}
