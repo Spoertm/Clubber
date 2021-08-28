6b0883392c319d7031a1d665c1acb630fceac5f0
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-08-28 22:36:35 +0200

    Implement full unit testing for UserService

--- Clubber.Tests/ServicesTests/UserServiceTests.cs
+++ Clubber.Tests/ServicesTests/UserServiceTests.cs
@@ -1,5 +1,7 @@
-﻿using Clubber.Configuration;
+﻿using System.Collections.Generic;
+using Clubber.Configuration;
 using Clubber.Helpers;
+using Clubber.Models;
 using Clubber.Models.Responses;
 using Clubber.Services;
 using Discord;
@@ -11,8 +13,9 @@ namespace Clubber.Tests.ServicesTests
 	public class UserServiceTests
 	{
 		private readonly UserService _sut;
-		private readonly Mock<Config> _configMock;
+		private readonly Mock<IConfig> _configMock;
 		private readonly Mock<IDatabaseHelper> _databaseHelperMock;
+		private const ulong CheaterRoleId = 666;
 
 		public UserServiceTests()
 		{
@@ -21,13 +24,87 @@ namespace Clubber.Tests.ServicesTests
 			_sut = new(_configMock.Object, _databaseHelperMock.Object);
 		}
 
-		[Fact]
-		public void IsValidForRegistrationDetectsBot()
+		[Theory]
+		[InlineData(true, new ulong[] { 1, 2, 3 })]
+		[InlineData(true, new ulong[] { 1, 2, 3, CheaterRoleId })]
+		[InlineData(false, new ulong[] { 1, 2, 3, CheaterRoleId })]
+		public void IsValidForRegistrationDetectsTypeOfUser(bool isBot, IReadOnlyCollection<ulong> roleIds)
 		{
 			Mock<IGuildUser> guildUser = new();
-			guildUser.SetupGet(user => user.IsBot).Returns(true);
-			UserValidationResponse response = _sut.IsValidForRegistration(guildUser.Object, true);
-			Assert.True(response.IsError);
+			guildUser.SetupGet(user => user.IsBot).Returns(isBot);
+			guildUser.SetupGet(user => user.RoleIds).Returns(roleIds);
+			_configMock.SetupGet(cm => cm.CheaterRoleId).Returns(CheaterRoleId);
+			UserValidationResponse isValidForRegistrationResponse = _sut.IsValidForRegistration(guildUser.Object, true);
+			Assert.True(isValidForRegistrationResponse.IsError);
+		}
+
+		[Theory]
+		[InlineData(true, new ulong[] { 1, 2, 3 })]
+		[InlineData(true, new ulong[] { 1, 2, 3, CheaterRoleId })]
+		[InlineData(false, new ulong[] { 1, 2, 3, CheaterRoleId })]
+		public void IsValidDetectsTypeOfUser(bool isBot, IReadOnlyCollection<ulong> roleIds)
+		{
+			Mock<IGuildUser> guildUser = new();
+			guildUser.SetupGet(user => user.IsBot).Returns(isBot);
+			guildUser.SetupGet(user => user.RoleIds).Returns(roleIds);
+			_configMock.SetupGet(cm => cm.CheaterRoleId).Returns(CheaterRoleId);
+			UserValidationResponse isValidResponse = _sut.IsValid(guildUser.Object, true);
+			Assert.True(isValidResponse.IsError);
+		}
+
+		[Fact]
+		public void IsValidForRegistrationDetectsValidUser()
+		{
+			// Normal user (neither bot nor has cheaterRoleId)
+			Mock<IGuildUser> guildUser = new();
+			guildUser.SetupGet(user => user.IsBot).Returns(false);
+			guildUser.SetupGet(user => user.RoleIds).Returns(new ulong[] { 1, 2, 3 });
+
+			_databaseHelperMock.Setup(dbhm => dbhm.GetDdUserByDiscordId(123)).Returns(default(DdUser));
+			UserValidationResponse isValidForRegistrationResponse = _sut.IsValidForRegistration(guildUser.Object, true);
+			Assert.False(isValidForRegistrationResponse.IsError);
+		}
+
+		[Fact]
+		public void IsValidForRegistrationDetectsAlreadyRegisteredUser()
+		{
+			// Normal user (neither bot nor has cheaterRoleId)
+			Mock<IGuildUser> guildUser = new();
+			guildUser.SetupGet(user => user.IsBot).Returns(false);
+			guildUser.SetupGet(user => user.Id).Returns(123);
+			guildUser.SetupGet(user => user.RoleIds).Returns(new ulong[] { 1, 2, 3 });
+
+			_databaseHelperMock.Setup(dbhm => dbhm.GetDdUserByDiscordId(123)).Returns(new DdUser(123, 123));
+			UserValidationResponse isValidForRegistrationResponse = _sut.IsValidForRegistration(guildUser.Object, true);
+			Assert.True(isValidForRegistrationResponse.IsError);
+		}
+
+		[Fact]
+		public void IsValidDetectsRegisteredUser()
+		{
+			// Normal user (neither bot nor has cheaterRoleId)
+			Mock<IGuildUser> guildUser = new();
+			guildUser.SetupGet(user => user.IsBot).Returns(false);
+			guildUser.SetupGet(user => user.Id).Returns(123);
+			guildUser.SetupGet(user => user.RoleIds).Returns(new ulong[] { 1, 2, 3 });
+
+			_databaseHelperMock.Setup(dbhm => dbhm.GetDdUserByDiscordId(123)).Returns(new DdUser(123, 123));
+			UserValidationResponse isValid = _sut.IsValid(guildUser.Object, true);
+			Assert.False(isValid.IsError);
+		}
+
+		[Fact]
+		public void IsValidDetectsUnregisteredNormalUser()
+		{
+			// Normal user (neither bot nor has cheaterRoleId)
+			Mock<IGuildUser> guildUser = new();
+			guildUser.SetupGet(user => user.IsBot).Returns(false);
+			guildUser.SetupGet(user => user.Id).Returns(123);
+			guildUser.SetupGet(user => user.RoleIds).Returns(new ulong[] { 1, 2, 3 });
+
+			_databaseHelperMock.Setup(dbhm => dbhm.GetDdUserByDiscordId(123)).Returns(default(DdUser));
+			UserValidationResponse isValid = _sut.IsValid(guildUser.Object, true);
+			Assert.True(isValid.IsError);
 		}
 	}
 }
