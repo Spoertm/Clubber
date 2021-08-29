5fa80a31ff08639cf4966c029d011bce66d5b31d
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-08-29 23:35:21 +0200

    Implement editnewspost command

---
+++ Clubber/Modules/Moderator.cs
@@ -0,0 +1,76 @@
+﻿using Clubber.Configuration;
+using Clubber.Helpers;
+using Clubber.Preconditions;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clubber.Modules
+{
+	[Name("Moderator")]
+	[Summary("Obtains user from their leaderboard ID and adds them to the database.")]
+	[RequireRole(697777821954736179, ErrorMessage = "Only moderators can use this command.")]
+	[RequireContext(ContextType.Guild)]
+	public class Moderator : ExtendedModulebase<SocketCommandContext>
+	{
+		private readonly IConfig _config;
+		private readonly IDiscordHelper _discordHelper;
+
+		public Moderator(IConfig config, IDiscordHelper discordHelper)
+		{
+			_config = config;
+			_discordHelper = discordHelper;
+		}
+
+		[Command("editnewspost")]
+		[Summary("Allows you to edit a DD news post made by the bot. If not message ID is given, then the latest post will be edited.")]
+		[Remarks("editnewspost 123456789 This is the new text!")]
+		[Priority(1)]
+		public async Task EditDdNewsPost([Name("message ID")] ulong messageId, [Name("text")][Remainder] string newMessage)
+		{
+			if (await IsError(string.IsNullOrWhiteSpace(newMessage), "Message can't be empty."))
+				return;
+
+			SocketTextChannel ddnewsPostChannel = _discordHelper.GetTextChannel(_config.DdNewsChannelId);
+			if (await ddnewsPostChannel.GetMessageAsync(messageId) is not IUserMessage messageToEdit)
+			{
+				await InlineReplyAsync("Could not find message.");
+				return;
+			}
+
+			if (await IsError(messageToEdit.Author.Id != Context.Client.CurrentUser.Id, "That message wasn't posted by the bot."))
+				return;
+
+			await messageToEdit.ModifyAsync(m => m.Content = newMessage);
+			await ReplyAsync("✅ Done!");
+		}
+
+		[Command("editnewspost")]
+		[Summary("Allows you to edit a DD news post made by the bot. If not message ID is given, then the latest post will be edited.")]
+		[Remarks("editnewspost This is the new text!")]
+		[Priority(0)]
+		public async Task EditDdNewsPost([Name("text")][Remainder] string newMessage)
+		{
+			if (await IsError(string.IsNullOrWhiteSpace(newMessage), "Message can't be empty."))
+				return;
+
+			SocketTextChannel ddnewsPostChannel = _discordHelper.GetTextChannel(_config.DdNewsChannelId);
+			IEnumerable<IMessage> messages = await ddnewsPostChannel.GetMessagesAsync(5).FlattenAsync();
+			IUserMessage? messageToEdit = messages.Where(m => m.Author.Id == Context.Client.CurrentUser.Id)
+				.OrderBy(m => m.CreatedAt.Date)
+				.FirstOrDefault() as IUserMessage;
+
+			if (messageToEdit is null)
+			{
+				await InlineReplyAsync("Could not find message.");
+				return;
+			}
+
+			await messageToEdit.ModifyAsync(m => m.Content = newMessage);
+			await ReplyAsync("✅ Done!");
+		}
+	}
+}
---
+++ Clubber/Preconditions/RequireRoleAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clubber.Preconditions
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+	public class RequireRoleAttribute : PreconditionAttribute
+	{
+		private readonly ulong _requiredRoleId;
+
+		public RequireRoleAttribute(ulong requiredRoleId) => _requiredRoleId = requiredRoleId;
+
+		public override string ErrorMessage { get; set; } = string.Empty;
+
+		public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+		{
+			if (context.User is not IGuildUser guildUser)
+				return Task.FromResult(PreconditionResult.FromError("Command has to be executed in a guild channel."));
+
+			if (guildUser.GuildPermissions.Administrator || guildUser.RoleIds.Contains(_requiredRoleId))
+				return Task.FromResult(PreconditionResult.FromSuccess());
+
+			IRole? requiredRole = context.Guild.GetRole(_requiredRoleId);
+			return Task.FromResult(PreconditionResult.FromError(ErrorMessage.Length == 0 ? $"Only users with {requiredRole.Name} role can use this command." : ErrorMessage));
+		}
+	}
+}
