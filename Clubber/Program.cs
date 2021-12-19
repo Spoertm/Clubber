73d9523882570136a6c527ddf16112b4ea502d33
Spoertm <spoertm@gmail.com>
2021-12-19 23:23:25 +0100

    Convert to web application, add API endpoints

--- Clubber/Program.cs
+++ Clubber/Program.cs
@@ -4,7 +4,8 @@ using Clubber.Services;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -29,22 +30,30 @@ namespace Clubber
 			DiscordSocketClient client = new(new() { AlwaysDownloadUsers = true, ExclusiveBulkDelete = true, LogLevel = LogSeverity.Error });
 			CommandService commands = new(new() { IgnoreExtraArgs = true, CaseSensitiveCommands = false, DefaultRunMode = RunMode.Async });
 
-			IHost host = ConfigureServices(client, commands).Build();
-			IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+			WebApplication app = ConfigureServices(client, commands).Build();
 
-			await client.LoginAsync(TokenType.Bot, config["BotToken"]);
+			app.UseSwagger();
+			app.UseSwaggerUI();
+
+			RegisterEndpoints(app);
+
+			await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("BotToken"));
 			await client.StartAsync();
 			await client.SetGameAsync("your roles", null, ActivityType.Watching);
-			await commands.AddModulesAsync(Assembly.GetEntryAssembly(), host.Services);
+			await commands.AddModulesAsync(Assembly.GetEntryAssembly(), app.Services);
 
-			host.Services.GetRequiredService<MessageHandlerService>();
-			host.Services.GetRequiredService<IDatabaseHelper>();
-			host.Services.GetRequiredService<WelcomeMessage>();
-			host.Services.GetRequiredService<LoggingService>();
+			app.Services.GetRequiredService<MessageHandlerService>();
+			app.Services.GetRequiredService<IDatabaseHelper>();
+			app.Services.GetRequiredService<WelcomeMessage>();
+			app.Services.GetRequiredService<LoggingService>();
+
+			app.UseHttpsRedirection();
+			app.UseAuthorization();
+			app.MapControllers();
 
 			try
 			{
-				await host.RunAsync(_source.Token);
+				await app.RunAsync(_source.Token);
 			}
 			catch (TaskCanceledException)
 			{
@@ -58,25 +67,49 @@ namespace Clubber
 			}
 		}
 
-		private static IHostBuilder ConfigureServices(DiscordSocketClient client, CommandService commands)
-			=> Host.CreateDefaultBuilder()
-				.ConfigureAppConfiguration((_, config) => config.AddJsonFile(Path.Combine(AppContext.BaseDirectory, "appsettings.json")))
-				.ConfigureServices(services =>
-					services.AddSingleton(client)
-						.AddSingleton(commands)
-						.AddSingleton<MessageHandlerService>()
-						.AddSingleton<IDatabaseHelper, DatabaseHelper>()
-						.AddSingleton<UpdateRolesHelper>()
-						.AddSingleton<IDiscordHelper, DiscordHelper>()
-						.AddSingleton<UserService>()
-						.AddSingleton<IWebService, WebService>()
-						.AddSingleton<LoggingService>()
-						.AddSingleton<WelcomeMessage>()
-						.AddSingleton<ImageGenerator>()
-						.AddHostedService<DdNewsPostService>()
-						.AddHostedService<DatabaseUpdateService>()
-						.AddDbContext<DatabaseService>())
-				.ConfigureLogging(logging => logging.ClearProviders());
+		private static void RegisterEndpoints(WebApplication app)
+		{
+			app.MapGet("/", async context =>
+			{
+				string indexHtmlPath = Path.Combine(AppContext.BaseDirectory, "Data", "Pages", "Index.html");
+				string indexHtml = await File.ReadAllTextAsync(indexHtmlPath);
+				await context.Response.WriteAsync(indexHtml);
+			});
+
+			app.MapGet("/users", (IDatabaseHelper databaseHelper)
+				=> databaseHelper.DdUserDatabase);
+
+			app.MapGet("/users/by-leaderboardId", (int leaderboardId, IDatabaseHelper databaseHelper)
+				=> databaseHelper.DdUserDatabase.Find(user => user.LeaderboardId == leaderboardId));
+
+			app.MapGet("/users/by-discordId", (ulong discordId, IDatabaseHelper databaseHelper)
+				=> databaseHelper.DdUserDatabase.Find(user => user.DiscordId == discordId));
+		}
+
+		private static WebApplicationBuilder ConfigureServices(DiscordSocketClient client, CommandService commands)
+		{
+			WebApplicationBuilder builder = WebApplication.CreateBuilder();
+			builder.Logging.ClearProviders();
+			builder.Services.AddControllers();
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+			builder.Services.AddSingleton(client)
+				.AddSingleton(commands)
+				.AddSingleton<MessageHandlerService>()
+				.AddSingleton<IDatabaseHelper, DatabaseHelper>()
+				.AddSingleton<UpdateRolesHelper>()
+				.AddSingleton<IDiscordHelper, DiscordHelper>()
+				.AddSingleton<UserService>()
+				.AddSingleton<IWebService, WebService>()
+				.AddSingleton<LoggingService>()
+				.AddSingleton<WelcomeMessage>()
+				.AddSingleton<ImageGenerator>()
+				.AddHostedService<DdNewsPostService>()
+				.AddHostedService<DatabaseUpdateService>()
+				.AddDbContext<DatabaseService>();
+
+			return builder;
+		}
 
 		private static void StopBot(object? sender, EventArgs e) => StopBot();
 
