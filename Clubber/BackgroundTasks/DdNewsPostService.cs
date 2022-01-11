aca61c0b7d20c051632017154037540440b9677c
Spoertm <spoertm@gmail.com>
2022-01-11 21:00:00 +0100

    Temporary hacky bug fix

--- Clubber/BackgroundTasks/DdNewsPostService.cs
+++ Clubber/BackgroundTasks/DdNewsPostService.cs
@@ -68,11 +68,11 @@ public class DdNewsPostService : AbstractBackgroundService
 				continue;
 
 			int nth = newEntries.Count(entry => entry.Time / 1000000 >= newEntry.Time / 1000000);
-			await _databaseHelper.AddDdNewsItem(oldEntry, newEntry, nth);
 			string message = GetDdNewsMessage(oldEntry, newEntry, nth);
 			string countryCode = await _webService.GetCountryCodeForplayer(newEntry.Id);
 			await using MemoryStream screenshot = await _imageGenerator.FromEntryResponse(newEntry, countryCode);
 			await _ddNewsChannel.SendFileAsync(screenshot, $"{newEntry.Username}_{newEntry.Time}.png", message);
+			await _databaseHelper.AddDdNewsItem(oldEntry, newEntry, nth);
 		}
 
 		if (cacheIsToBeRefreshed)
