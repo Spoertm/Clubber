8801e14588470c109912ba23ad91a7409afd742b
Spoertm <spoertm@gmail.com>
2022-04-25 23:30:31 +0200

    Fix bug

--- Clubber/Helpers/RunAnalyzer.cs
+++ Clubber/Helpers/RunAnalyzer.cs
@@ -20,7 +20,7 @@ public static class RunAnalyzer
 			if (startOfSplitState is null || endOfSplitState is null)
 				continue;
 
-			int splitValue = endOfSplitState.HomingDaggers + startOfSplitState.HomingDaggers;
+			int splitValue = endOfSplitState.HomingDaggers - startOfSplitState.HomingDaggers;
 			if (nextSplit.Name == "350")
 				splitValue -= 105;
 
