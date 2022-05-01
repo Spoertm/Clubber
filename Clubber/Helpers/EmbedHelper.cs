4f0ea37e14f018af6b6985034f7abe9dc1b0a432
Spoertm <spoertm@gmail.com>
2022-05-01 14:51:25 +0200

    In-game ID only

--- Clubber/Helpers/EmbedHelper.cs
+++ Clubber/Helpers/EmbedHelper.cs
@@ -199,7 +199,7 @@ public static class EmbedHelper
 	{
 		const string registerForRolesText = @"This is a bot related to the game Devil Daggers. We have roles corresponding to in-game scores ranging from <@&461203024128376832> to <@&903024433315323915>.
 
-If you'd like to have a role and be able to do stuff like in the image below, feel free to register by posting your in-game name or ID - which you can get from [devildaggers.info](https://devildaggers.info/Leaderboard) (*hover over your rank and it should appear*).
+If you'd like to have a role and be able to do stuff like in the image below, feel free to register by posting your in-game ID - which you can get from [devildaggers.info](https://devildaggers.info/Leaderboard) (*hover over your rank and it should appear*).
 
 If you don't play the game or simply don't want to be registered, post ""`no score`"".
 
