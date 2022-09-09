a3a017cecbf51451f3175eba5d49a718bab614bf
Spoertm <spoertm@gmail.com>
2022-09-09 23:47:18 +0200

    Update ExtentionMethodsTests.cs

--- Clubber.Tests/ExtentionMethodsTests.cs
+++ Clubber.Tests/ExtentionMethodsTests.cs
@@ -10,7 +10,9 @@ public class ExtentionMethodsTests
 	[InlineData(2, "nd")]
 	[InlineData(3, "rd")]
 	[InlineData(4, "th")]
-	[InlineData(128, "th")]
+	[InlineData(11, "th")]
+	[InlineData(12, "th")]
+	[InlineData(13, "th")]
 	public void OrdinalIndicator_GetsNumber_ReturnsCorrectOrdinalIndicator(int number, string expectedOrdinalIndicator)
 	{
 		Assert.Equal(number.OrdinalIndicator(), expectedOrdinalIndicator);
