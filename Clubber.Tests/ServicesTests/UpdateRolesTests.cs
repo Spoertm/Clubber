ea928a9ca5c3607ba27126bd2c39a47b2d54b886
Spoertm <spoertm@gmail.com>
2022-09-09 23:41:15 +0200

    Update test for new roles

--- Clubber.Tests/ServicesTests/UpdateRolesTests.cs
+++ Clubber.Tests/ServicesTests/UpdateRolesTests.cs
@@ -15,6 +15,7 @@ public class UpdateRolesTests
 	private const ulong OneHundredRoleId = 399569183966363648;
 	private const ulong ThreeHundredRoleId = 399569332532674562;
 	private const ulong TwelveThirtyRoleId = 903024433315323915;
+	private const ulong TwelveFiftyRoleId = 980126799075876874;
 	private const ulong NineHundredRoleId = 399570895741386765;
 	private const ulong Top1RoleId = 446688666325090310;
 	private const ulong Top3RoleId = 472451008342261820;
@@ -32,7 +33,7 @@ public class UpdateRolesTests
 	[Theory]
 	[InlineData(0, new ulong[] { }, SubOneHundredRoleId, new ulong[] { })]
 	[InlineData(100 * 10000, new ulong[] { }, OneHundredRoleId, new ulong[] { })]
-	[InlineData(1500 * 10000, new ulong[] { }, TwelveThirtyRoleId, new ulong[] { })]
+	[InlineData(1500 * 10000, new ulong[] { }, TwelveFiftyRoleId, new ulong[] { })]
 	[InlineData(100 * 10000, new[] { OneHundredRoleId, ThreeHundredRoleId }, 0, new[] { ThreeHundredRoleId })]
 	[InlineData(900 * 10000, new[] { OneHundredRoleId, ThreeHundredRoleId, TwelveThirtyRoleId }, NineHundredRoleId, new[] { OneHundredRoleId, ThreeHundredRoleId, TwelveThirtyRoleId })]
 	public void HandleScoreRoles_DetectsRoleInconsistency_ReturnsRolesToBeAddedAndRemoved(
