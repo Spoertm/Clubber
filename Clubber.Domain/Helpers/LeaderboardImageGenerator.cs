d3e05ef218bed4bb79b7aa1a75b494906d43cf49
Spoertm <spoertm@gmail.com>
2023-04-10 14:13:35 +0200

    Update packages

--- Clubber.Domain/Helpers/LeaderboardImageGenerator.cs
+++ Clubber.Domain/Helpers/LeaderboardImageGenerator.cs
@@ -1,9 +1,6 @@
 using Clubber.Domain.Extensions;
 using SixLabors.Fonts;
-using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing.Processing;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
 
 namespace Clubber.Domain.Helpers;
 
