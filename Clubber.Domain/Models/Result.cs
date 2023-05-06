ae35008c35363930f4832f76cc144b539561a2a5
Spoertm <spoertm@gmail.com>
2023-05-06 15:09:32 +0200

    Use file-scroped namespaces

--- Clubber.Domain/Models/Result.cs
+++ Clubber.Domain/Models/Result.cs
@@ -1,40 +1,39 @@
-﻿namespace Clubber.Domain.Models
+﻿namespace Clubber.Domain.Models;
+
+public class Result
 {
-	public class Result
+	protected Result(bool isSuccess, string errorMsg)
 	{
-		protected Result(bool isSuccess, string errorMsg)
-		{
-			if (isSuccess && errorMsg != string.Empty)
-				throw new InvalidOperationException();
+		if (isSuccess && errorMsg != string.Empty)
+			throw new InvalidOperationException();
 
-			if (!isSuccess && errorMsg == string.Empty)
-				throw new InvalidOperationException();
+		if (!isSuccess && errorMsg == string.Empty)
+			throw new InvalidOperationException();
 
-			IsSuccess = isSuccess;
-			ErrorMsg = errorMsg;
-		}
-
-		public bool IsSuccess { get; }
-		public string ErrorMsg { get; }
-		public bool IsFailure => !IsSuccess;
-
-		public static Result Failure(string message) => new(false, message);
-
-		public static Result<T?> Failure<T>(string message) => new(default, false, message);
-
-		public static Result Success() => new(true, string.Empty);
-
-		public static Result<T> Success<T>(T value) => new(value, true, string.Empty);
+		IsSuccess = isSuccess;
+		ErrorMsg = errorMsg;
 	}
 
-	public class Result<T> : Result
-	{
-		protected internal Result(T value, bool isSuccess, string errorMsg)
-			: base(isSuccess, errorMsg)
-		{
-			Value = value;
-		}
+	public bool IsSuccess { get; }
+	public string ErrorMsg { get; }
+	public bool IsFailure => !IsSuccess;
 
-		public T Value { get; }
+	public static Result Failure(string message) => new(false, message);
+
+	public static Result<T?> Failure<T>(string message) => new(default, false, message);
+
+	public static Result Success() => new(true, string.Empty);
+
+	public static Result<T> Success<T>(T value) => new(value, true, string.Empty);
+}
+
+public class Result<T> : Result
+{
+	protected internal Result(T value, bool isSuccess, string errorMsg)
+		: base(isSuccess, errorMsg)
+	{
+		Value = value;
 	}
+
+	public T Value { get; }
 }
