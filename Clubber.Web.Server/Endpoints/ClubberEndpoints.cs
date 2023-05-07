fe9d78c0683760b733ed0495db52cf73c56fe0a1
Spoertm <spoertm@gmail.com>
2023-05-07 14:33:25 +0200

    Use Async scope, use FindAsync where possible

--- Clubber.Web.Server/Endpoints/ClubberEndpoints.cs
+++ Clubber.Web.Server/Endpoints/ClubberEndpoints.cs
@@ -26,37 +26,37 @@ public static class ClubberEndpoints
 
 	private static async Task<BestSplit?> BestSplitByName(string splitName, IServiceScopeFactory scopeFactory)
 	{
-		using IServiceScope scope = scopeFactory.CreateScope();
+		await using AsyncServiceScope scope = scopeFactory.CreateAsyncScope();
 		await using DbService dbContext = scope.ServiceProvider.GetRequiredService<DbService>();
-		return await dbContext.BestSplits.AsNoTracking().FirstOrDefaultAsync(bs => bs.Name == splitName);
+		return await dbContext.BestSplits.FindAsync(splitName);
 	}
 
-	private static async Task<BestSplit[]> BestSplits(IServiceScopeFactory scopeFactory)
+	private static async Task<List<BestSplit>> BestSplits(IServiceScopeFactory scopeFactory)
 	{
-		using IServiceScope scope = scopeFactory.CreateScope();
+		await using AsyncServiceScope scope = scopeFactory.CreateAsyncScope();
 		await using DbService dbContext = scope.ServiceProvider.GetRequiredService<DbService>();
-		return await dbContext.BestSplits.AsNoTracking().ToArrayAsync();
+		return await dbContext.BestSplits.AsNoTracking().ToListAsync();
 	}
 
 	private static async Task<List<DdNewsItem>> DailyNews(IServiceScopeFactory scopeFactory)
 	{
-		using IServiceScope scope = scopeFactory.CreateScope();
+		await using AsyncServiceScope scope = scopeFactory.CreateAsyncScope();
 		await using DbService dbContext = scope.ServiceProvider.GetRequiredService<DbService>();
 		return await dbContext.DdNews.AsNoTracking().ToListAsync();
 	}
 
 	private static async Task<DdUser?> UserByDiscordId(ulong discordId, IServiceScopeFactory scopeFactory)
 	{
-		using IServiceScope scope = scopeFactory.CreateScope();
+		await using AsyncServiceScope scope = scopeFactory.CreateAsyncScope();
 		await using DbService dbContext = scope.ServiceProvider.GetRequiredService<DbService>();
 		return await dbContext.DdPlayers.AsNoTracking().FirstOrDefaultAsync(user => user.DiscordId == discordId);
 	}
 
 	private static async Task<DdUser?> UserByLeaderboardId(int leaderboardId, IServiceScopeFactory scopeFactory)
 	{
-		using IServiceScope scope = scopeFactory.CreateScope();
+		await using AsyncServiceScope scope = scopeFactory.CreateAsyncScope();
 		await using DbService dbContext = scope.ServiceProvider.GetRequiredService<DbService>();
-		return await dbContext.DdPlayers.AsNoTracking().FirstOrDefaultAsync(user => user.LeaderboardId == leaderboardId);
+		return await dbContext.DdPlayers.FindAsync(leaderboardId);
 	}
 
 	private static async Task<List<DdUser>> RegisteredUsers(IDatabaseHelper dbhelper)
