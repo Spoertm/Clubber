7fc3404808d26f3a69adfb3993a7afc9acdfde84
Spoertm <spoertm@gmail.com>
2023-05-21 01:13:49 +0200

    Implement PB datetime in stats embed

---
+++ Clubber.Domain/Models/Responses/DdInfo/GetPlayerHistoryActivityEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace Clubber.Domain.Models.Responses.DdInfo;
+
+public record GetPlayerHistoryActivityEntry
+{
+	public required DateTime DateTime { get; init; }
+
+	public required double DeathsIncrement { get; init; }
+
+	public required double TimeIncrement { get; init; }
+}
