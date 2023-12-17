35d40a928fac9e6453b8367f6e79bd56730c92db
Spoertm <spoertm@gmail.com>
2023-12-17 19:35:46 +0100

    Make service more resilient to errors

--- Clubber.Domain/BackgroundTasks/KeepAppAliveService.cs
+++ Clubber.Domain/BackgroundTasks/KeepAppAliveService.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 namespace Clubber.Domain.BackgroundTasks;
 
 public class KeepAppAliveService : AbstractBackgroundService
@@ -13,7 +15,20 @@ public class KeepAppAliveService : AbstractBackgroundService
 
 	protected override async Task ExecuteTaskAsync(CancellationToken stoppingToken)
 	{
-		const string appUrl = "https://clubber.onrender.com";
-		await _httpClientFactory.CreateClient().GetStringAsync(appUrl, stoppingToken);
+		const string envVarName = "AppUrl";
+		if (Environment.GetEnvironmentVariable(envVarName) is not { } appUrl)
+		{
+			Log.Warning("{EnvVarName} environment variable not set", envVarName);
+			return;
+		}
+
+		try
+		{
+			await _httpClientFactory.CreateClient().GetStringAsync(appUrl, stoppingToken).ConfigureAwait(false);
+		}
+		catch (Exception e)
+		{
+			Log.Error(e, "Failed to ping {AppUrl}", appUrl);
+		}
 	}
 }
