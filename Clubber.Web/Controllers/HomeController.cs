5f1245283d90caa2ec58d41d3f2e05334bca62e9
Spoertm <spoertm@gmail.com>
2025-06-01 14:37:29 +0200

    Fix controller visibility bug

--- Clubber.Web/Controllers/HomeController.cs
+++ Clubber.Web/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace Clubber.Web.Controllers;
 
-internal sealed class HomeController : Controller
+public sealed class HomeController : Controller
 {
 	public IActionResult Index()
 	{
