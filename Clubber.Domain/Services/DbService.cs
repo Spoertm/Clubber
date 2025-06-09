9364f1b23bbb657f2316e940bd67f38650434ccb
Spoertm <spoertm@gmail.com>
2025-06-09 15:39:11 +0200

    Update DbService.cs

--- Clubber.Domain/Services/DbService.cs
+++ Clubber.Domain/Services/DbService.cs
@@ -23,8 +23,6 @@ public class DbService : DbContext
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
-		modelBuilder.HasDefaultSchema("clubber");
-
 		modelBuilder.Entity<DdNewsItem>().Property(ddni => ddni.OldEntry).HasColumnType("jsonb");
 		modelBuilder.Entity<DdNewsItem>().Property(ddni => ddni.NewEntry).HasColumnType("jsonb");
 
